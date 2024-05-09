12021fc17f16be226d4cd1c09cc08f5d4cba1587
Istoleyourbutter <tinderisgay420@gmail.com>
2024-05-09 15:43:41 -0400

    Add files via upload

--- install.py
+++ install.py
@@ -14,7 +14,7 @@ def install_system_dependencies():
     system = platform.system().lower()
     if system == "linux":
         if os.path.exists("/etc/arch-release"):
-            subprocess.run(["sudo", "pacman", "-S", "--noconfirm", "python-pyqt5"], check=True)
+            subprocess.run(["sudo", "pacman", "-S", "--noconfirm", "python-pyqt5", "python-yarl"], check=True)
         else:
             subprocess.run(["sudo", "apt-get", "install", "-y", "python3-pyqt5"], check=True)
     elif system == "darwin":
@@ -30,16 +30,24 @@ def setup_maigret():
     try:
         os.system("rmdir /s /q src\\maigret")
         subprocess.run(["git", "clone", "https://github.com/soxoj/maigret", "src\\maigret"], check=True)
-        subprocess.run(["pip", "install", "-r", "src\\maigret\\requirements.txt", "--break-system-packages"], check=True)
-        subprocess.run(["pip", "install", "src\\maigret", "--break-system-packages"], check=True)
-        print("Maigret setup completed successfully.")
+        if os.path.exists("src/maigret/requirements.txt"):
+            subprocess.run(["pip", "install", "-r", "src\\maigret\\requirements.txt", "--break-system-packages"], check=True)
+            subprocess.run(["pip", "install", "src\\maigret", "--break-system-packages"], check=True)
+            print("Maigret setup completed successfully.")
+        else:
+            print("Error: requirements.txt not found.")
     except subprocess.CalledProcessError:
         print("Error setting up Maigret.")
 
 def install_yarl():
     try:
-        subprocess.run(["pip", "install", "yarl"], check=True)
-        print("yarl installed successfully.")
+        system = platform.system().lower()
+        if system == "linux" and os.path.exists("/etc/arch-release"):
+            subprocess.run(["sudo", "pacman", "-S", "--noconfirm", "python-yarl"], check=True)
+            print("python-yarl installed successfully.")
+        else:
+            subprocess.run(["pip", "install", "yarl"], check=True)
+            print("yarl installed successfully.")
     except subprocess.CalledProcessError:
         print("Error installing yarl.")
 
@@ -48,3 +56,4 @@ if __name__ == "__main__":
     install_dependencies()
     install_yarl()
     setup_maigret()
+
